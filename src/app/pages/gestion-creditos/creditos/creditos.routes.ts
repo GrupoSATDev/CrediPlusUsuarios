574af2a1e993aba2e2c26fee16c4641a2039cdfc
Mijail Gonzalez <mijail0824@gmail.com>
2024-10-23 22:31:42 -0500

    [Sprint 10] refactor

--- src/app/pages/gestion-creditos/creditos/creditos.routes.ts
+++ src/app/pages/gestion-creditos/creditos/creditos.routes.ts
@@ -1,16 +1,10 @@
 import { Routes } from '@angular/router';
-import { GridCreditosComponent } from './grid-creditos/grid-creditos.component';
-import { FormDetalleComponent } from './form-detalle/form-detalle.component';
 import { FormViewDetalleComponent } from './form-view-detalle/form-view-detalle.component';
 
 export default [
     {
         path: '',
         component: FormViewDetalleComponent
-    },
-    {
-        path: 'detalle/:id',
-        component: FormDetalleComponent
     },
     {
         path: 'view-detalle/:id',
--- src/app/pages/gestion-creditos/gestion-creditos.routes.ts
+++ src/app/pages/gestion-creditos/gestion-creditos.routes.ts
@@ -9,24 +9,10 @@ export default [
                 loadChildren: () => import('./solicitudes/solicitudes.routes')
             },
 
-
             {
                 path: 'creditos',
                 loadChildren: () => import('./creditos/creditos.routes')
             },
-            {
-                path: 'detalle-consumo',
-                loadChildren: () => import('./detalle-consumo/detalle-consumo.routes')
-            },
-            {
-                path: 'consumos',
-                loadChildren: () => import('./consumos/consumos.routes')
-            },
-            {
-                path: 'desembolsos',
-                loadChildren: () => import('./desembolsos/desembolsos.routes')
-            },
-
         ]
     }
 
