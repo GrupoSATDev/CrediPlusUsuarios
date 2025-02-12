0ad9c6ae72352a75c4638e210375563a01114a91
Mijail Gonzalez <49327662+mgonzalez0889@users.noreply.github.com>
2025-02-12 17:30:39 -0500

    [Sprint 12] reload infinito empleados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,7 +10,7 @@ import { LayoutComponent } from 'app/layout/layout.component';
 export const appRoutes: Route[] = [
 
     // Redirect empty path to '/example'
-    {path: '', pathMatch : 'full', redirectTo: 'sign-in'},
+    {path: '', pathMatch : 'full', redirectTo: 'pages/gestion-creditos/solicitudes'},
 
     // Redirect signed-in user to the '/example'
     //
@@ -29,9 +29,6 @@ export const appRoutes: Route[] = [
             layout: 'empty'
         },
         children: [
-            {path: 'confirmation-required', loadChildren: () => import('app/modules/auth/confirmation-required/confirmation-required.routes')},
-            {path: 'forgot-password', loadChildren: () => import('app/modules/auth/forgot-password/forgot-password.routes')},
-            {path: 'reset-password', loadChildren: () => import('app/modules/auth/reset-password/reset-password.routes')},
             {path: 'sign-in', loadChildren: () => import('app/modules/auth/sign-in/sign-in.routes')},
             {path: 'sign-up', loadChildren: () => import('app/modules/auth/sign-up/sign-up.routes')}
         ]
